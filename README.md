031604d348d3d4cd8726393b06cb02c15b677684
samuel-stuba <55798553+samuel-stuba@users.noreply.github.com>
2020-04-01 15:58:14 +0200

    main.js: vytvorenie triedy Paratrooper, rozpohybovanie objektu, vymazanie zbytocneho kodu; pridanie obrazku crosshair.png

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ZTIAP hra Paratrooper - Samuel Piják
 
 - commit hlavneho html suboru a priecinku img s obrazkami
 - vytvorenie suboru main.js a prepisanie index.html aby vykonaval script main.js
+- premenovanie suboru index.html na paratrooper.html
+- main.js: vytvorenie triedy Paratrooper; vymazanie zbytocneho kodu; rozpohybovanie postavy paratrooper; pridanie obrazku crosshair.png do dir img
--- main.js
+++ main.js
@@ -1,78 +1,38 @@
-var canvas = document.getElementById("canvas")
-var context = canvas.getContext("2d")
-var y = 0
-var x = 600
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-var bg = new Image()
-bg.src = "img/background.png"
-bg.onload = animate
+class Paratrooper {
+  constructor() {
+    this.canvas = document.getElementById("canvas");
+    this.image = new Image();
+    this.image.src = "img/paratrooper.png";
 
-var para = new Image()
-para.onload = animate
-para.src = "img/paratrooper.png"
-
-var heli = new Image()
-heli.onload = animate
-heli.src = "img/heli.png"
-
-var bomb = new Image()
-bomb.onload = animate
-bomb.src = "img/bomb.png"
-
-function animate() {
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.drawImage(bg, 0, 0)
-  context.drawImage(para, 100, y)
-  context.drawImage(bomb, 0, y)
-  context.drawImage(heli, x, 50)
-  y += 1
-  x -= 1
-  requestAnimationFrame(animate)
-}
-
-/*class Obj {
-  constructor(x, y) {
-    this.image = document.getElementById("image")
-    this.x = x
-    this.y = y
+    this.x = Math.random() * canvas.width
+    this.y = 0
   }
 
   move() {
-    this.y += 10
+    const canvas = this.canvas;
+    if (this.y > canvas.height) {
+      this.y -= canvas.height
+    }
+
+    this.y += 5
   }
 
   draw() {
-    context.save()
-    context.translate(0, this.y)
-    context.drawImage(this.image, this.x, this.y)
-    context.restore()
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.drawImage(this.image, 0, 0, 120, 160)
+    ctx.restore()
   }
 }
-var para = new Obj(300, 0)
-para.draw()*/
 
-/*
-function draw() {
-  context.fillStyle = "white"
-  context.fillRect(0, 0, canvas.width, canvas.height)
+para = new Paratrooper()
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
   para.draw()
-}
-
-function move() {
   para.move()
+  requestAnimationFrame(animate)
 }
-
-var para = new Obj(300, 0, 290, 320)
-
-function step(){
-  move()
-  draw()
-  context.font = '30px Arial'
-  context.fillText('Paratrooper', 0, 25)
-  requestAnimationFrame(step)
-}
-window.onload = function() {
-  canvas = document.getElementById("canvas")
-  context = canvas.getContext("2d")
-  step()
-}*/
+animate()
